a644c896a01cd6f943c17c2421a0128341ee2d66
gband79 <gbandeira79@yahoo.com.br>
2021-02-22 09:58:30 -0300

    delete unecessary code from runner.java

--- TesteSelenium/src/test/java/com/accenture/runners/Runner.java
+++ TesteSelenium/src/test/java/com/accenture/runners/Runner.java
@@ -17,27 +17,6 @@ import cucumber.api.junit.Cucumber;
 		)
 public class Runner {
 	
-//	@Before
-//	public static void reset() {
-//	System.setProperty("webdriver.chrome.driver",
-//		"C:\\Windows\\chromedriver.exe");
-//	
-//	WebDriver driver = new ChromeDriver();
-//	driver.manage().window().maximize();
-//	driver.get("http://sampleapp.tricentis.com/101/app.php");
-//	
-//	driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
-//	WebDriver driver = new ChromeDriver();
-//	
-	
-//	driver.get("http://sampleapp.tricentis.com/101/app.php");
-//		
-//	driver.findElement(By.id("email")).sendKeys("eu@bandeira.io");
-//	driver.findElement(By.name("senha")).sendKeys("123");
-//	driver.findElement(By.cssSelector("button[type='submit']")).click();
-//	driver.findElement(By.cssSelector("a[href='/reset']")).click();
-//	driver.quit();
-//		
-//	}
+
 	
 }
